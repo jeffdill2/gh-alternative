3e6e927328857707abbb802dd144644f115237e6
Jeffrey Dill <jeffdill2@gmail.com>
2014-05-15 12:02:00 -0400

    Added authorization to API pulls to solve rate limit issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,12 @@
 ////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////// DATA
 ////////////////////////////////////////////////////////////
-var strGitHubURL = "https://api.github.com/users/jeffdill2";
 var sidebarTemplate = _.template($('#sidebar-template').text());
 var repoTemplate = _.template($('#repos-template').text());
+var strGitHubURL = "https://api.github.com/users/jeffdill2" + strAuthKey;
 
 $.getJSON(strGitHubURL).done(function(objUserData) {
-  $.getJSON([objUserData][0].repos_url).done(function(aryRepoData) {
+  $.getJSON([objUserData][0].repos_url + strAuthKey).done(function(aryRepoData) {
     renderSidebar([objUserData], aryRepoData);
     renderRepos(aryRepoData);
   });
